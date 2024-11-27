b7b0c68980c3c6d3428bcf42b9bf95c13861f528
Moses <asugam9@gmail.com>
2024-11-27 20:44:06 +0300

    pug intro

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express()
+
+app.set("view engine", "pug");
+app.set("views" , "./views");
+
+
+const PORT = 3000;
+app.use(express.static('./views/style.css'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res) =>{
+res.render('login')
+
+});
+app.post('/login', (req,res) =>{    
+    const {username,email,password} = req.body;
+    console.log(req.body.email);
+    console.log(req.body.password);
+    if(req.body.email==="johndoe@gmail.com" && req.body.password==="123456John"){
+     res.render('home')
+    } 
+    else{
+        res.send('invalid password or email')
+    }
+});
+
+
+// res.send()
+app.listen(PORT,(req,res) =>{
+    console.log('listening to port 3000')
+});
+
